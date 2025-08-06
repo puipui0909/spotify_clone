2d449f0856d026a2b6bb1671c798770f2bca2917
puipui0909 <bdung091203@gmail.com>
2025-08-06 20:55:45 +0700

    create screen choose app theme when first open the app

---
+++ lib/Screens/choose_theme_screen.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:spotify_clone/theme/theme_notifier.dart';
+
+class chooseThemeScreen extends StatelessWidget{
+  const chooseThemeScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    final themeNotifier = Provider.of<ThemeNotifier>(context);
+    final curentMode = themeNotifier.themeMode;
+    return Scaffold(
+      // backgroundColor: Colors.black,
+      body: Center(
+        child: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16),
+                child: Image.asset('assets/images/loading.png'),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 24.0),
+                child: Column(
+                  children: [
+                  Container(
+                    height: 177,
+                    width: 233,
+                    child: Column(
+                      children: [
+                        Text('Choose Mode',
+                          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25)),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Column(
+                              children: [
+                                IconButton(
+                                    icon: const Icon(Icons.light_mode),
+                                    onPressed: (){
+                                      themeNotifier.setTheme(ThemeMode.light);
+                                    },
+                                    iconSize: 50,
+                                ),
+                                Text('Light Mode',style: TextStyle(fontSize: 13),)
+                              ],
+                            ),
+                            Column(
+                              children: [
+                                IconButton(
+                                    icon: const Icon(Icons.dark_mode),
+                                    onPressed: (){
+                                      themeNotifier.setTheme(ThemeMode.dark);
+                                    },
+                                    iconSize: 50,
+                                ),
+                                Text('Dark Mode', style: TextStyle(fontSize: 13),)
+                              ],
+                            ),
+                           
+                                
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 10,),
+
+                  SizedBox(height: 50,),
+                  TextButton(
+                    onPressed: (){},
+                    style: TextButton.styleFrom(
+                      backgroundColor: Colors.green,
+                      foregroundColor: Colors.white,
+                      minimumSize: Size(329, 92),
+                      shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(25)
+                      )
+                    ),
+                  child: Text('Continue', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),))
+                ],
+                ),
+    )
+    ],
+    )
+    ),
+    )
+    );
+    }
+
+}

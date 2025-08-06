144850d81735a396187463a3263172caa3f802be
puipui0909 <bdung091203@gmail.com>
2025-08-06 20:54:50 +0700

    create screen choose register or signin for user

---
+++ lib/Screens/register_or_signin.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class RegisterOrSign extends StatelessWidget{
+  const RegisterOrSign({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Padding(
+            padding: EdgeInsets.all(8.0),
+            child: IconButton(
+                onPressed: (){
+
+                },
+                icon: Icon(Icons.arrow_back),),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(
+                  top: 45,
+                  bottom:30),
+              child: Image.asset('assets/images/loading.png', width: 235,height: 71),
+            ),
+            Text('Enjoy listening to music', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 26),),
+            SizedBox(height: 25,),
+            Container(
+              width: 306,
+              height: 46,
+              child: Text('Spotify is a proprietary Swedish audio streaming and media services provider',
+                style: TextStyle(fontSize: 17), textAlign: TextAlign.center, ),
+            ),
+            SizedBox(height: 25,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                    style: TextButton.styleFrom(
+                      minimumSize: Size(147, 73),
+                      backgroundColor: Colors.green,
+                      foregroundColor: Colors.white,
+                    ),
+                    onPressed: (){},
+                    child: Text('Register', style: TextStyle(fontSize: 19),)),
+                SizedBox(width: 30,),
+                TextButton(
+                    style: TextButton.styleFrom(
+                        minimumSize: Size(147, 73)
+                    ),
+                    onPressed: (){}, 
+                    child: Text('Sign in', style: TextStyle(fontSize: 19),))
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+}

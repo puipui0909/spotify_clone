09290f520bea2ba44cabeb79fe763789a4f988ba
puipui0909 <bdung091203@gmail.com>
2025-08-06 20:49:42 +0700

    connect to firebase

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,6 +5,19 @@ allprojects {
     }
 }
 
+buildscript {
+    dependencies {
+        classpath("com.google.gms:google-services:4.4.3")
+    }
+}
+
+plugins {
+    id("com.android.application") version "8.7.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.22" apply false
+    //id("com.google.gms.google-services") version "4.4.3" apply false
+
+}
+
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
@@ -19,3 +32,5 @@ subprojects {
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
+
+
